0a9b3d16caa4bdfe67930776c4bdd794d376c161
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-12-07 13:32:59 -0800

    Tip Calculator

---
+++ DAY 2/task.py
@@ -0,0 +1,7 @@
+#Tip Calculator
+print("Welcome to the tip calculator!")
+totalBill = float(input("What was the total bill? $"))
+tipPercent = int(input("How much tip would you like give? 10, 12, or 15 "))
+amountPeople = int(input("How many people to split the bill? "))
+pay = round(totalBill*(1 + tipPercent/100)/amountPeople, 2)
+print(f"Each person should pay: ${pay}")
