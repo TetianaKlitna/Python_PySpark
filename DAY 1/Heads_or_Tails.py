b3acc313e026e6a017cdb011a8975d9b1c8440d4
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-12-08 00:14:54 -0800

    Concepts

---
+++ DAY 1/Heads_or_Tails.py
@@ -0,0 +1,7 @@
+import random
+
+randomValue = random.randint(0,1)
+if randomValue == 0:
+    print("Heads")
+else:
+    print("Tails")
---
+++ DAY 1/Treasure_Island.py
@@ -0,0 +1,16 @@
+print("Welcome to Treasure Island.\nYour mission is to find the treasure")
+print("You're at a cross road. Where do you want to go?")
+direction = input("Type \"left\" or \"right\" ").strip().lower()
+if direction == "left":
+    print("You've come to a lake. There is an island in the middle of the lake.")
+    action = input("Type \"wait\" to wait for a boat. Type \"swim\" to swim across. ").strip().lower()
+    if action == "wait":
+        color = input("Which door? red, yellow, or blue ").strip().lower()
+        if color == "yellow":
+            print("You win!")
+        else:
+            print("Game over.")
+    else:
+        print("Game over.")
+else:
+    print("Game Over.")
