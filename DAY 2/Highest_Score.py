76c3014ad8b319ca256a8e3a9e0c4e8086eeed74
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-12-09 16:55:50 -0800

    Loop and range

---
+++ DAY 2/Highest_Score.py
@@ -0,0 +1,13 @@
+import math
+
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+# 1 option
+print(max(student_scores))
+# 2 second
+maxValue = 0
+for score in student_scores:
+    if score > maxValue:
+        maxValue = score
+print(maxValue)
+
+
