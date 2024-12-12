72d14568c4f623a55afd2a3507c0ca7fb2252a3e
Tetiana Klitna <klitna.tetiana@gmail.com>
2024-12-12 01:05:44 -0800

    Games

---
+++ DAY 3/Caesar_Cipher.py
@@ -0,0 +1,29 @@
+from Caesar_Art import logo
+
+print(logo)
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def caesar_cipher(original_text, shift_amount, encode_or_decode):
+    result_text = ""
+    size_alphabet = len(alphabet)
+    if encode_or_decode == 'decode':
+        shift_amount *= -1
+    for letter in original_text:
+        if letter in alphabet:
+            new_ind =  (alphabet.index(letter) + shift_amount) % size_alphabet
+            result_text += alphabet[new_ind]
+        else:
+            result_text += letter
+    print(f"Here is the {encode_or_decode}d result: {result_text}")
+
+should_continue = "yes"
+while should_continue == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caesar_cipher(original_text = text, shift_amount = shift, encode_or_decode = direction)
+    should_continue = input("Type 'yes' if you want to go again. Otherwise type 'no'\n").lower()
+    if should_continue == "no":
+        print("Goodbye")
---
+++ DAY 3/Life_In_Weeks_Calculator.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+def life_in_weeks(age):
+    start_date = date.today()
+    year = start_date.year
+    end_date = date(year, 12, 31)
+    extra_days = 0
+    full_weeks = 0
+    while age <= 90:
+        diff_days = (end_date - start_date).days + extra_days
+        full_weeks += diff_days//7
+        extra_days = diff_days%7
+        age += 1
+        year += 1
+        start_date = date(year, 1,1)
+        end_date = date(year, 12, 31)
+    print(f"You have {full_weeks} weeks until 90 years old")
+
+life_in_weeks(20)
